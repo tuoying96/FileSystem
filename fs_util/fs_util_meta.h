25e7a483fd737a7639de4d8318ae869c78a48ae4
Arthur_Cai <caiwenhanarthur@gmail.com>
2020-11-23 10:31:28 +0800

    Update fs_util_meta.h

--- fs_util/fs_util_meta.h
+++ fs_util/fs_util_meta.h
@@ -1,73 +1,182 @@
 /*
- * fs_metadata.h
+ * fs_util_file.h
  *
- * description: metadata utility functions for CS 5600 / 7600 file system
+ * description: file utility functions for CS 5600 / 7600 file system
  *
  * CS 5600, Computer Systems, Northeastern CCIS
  * Peter Desnoyers, November 2016
  * Philip Gust, March 2019, March 2020
  */
 
-#ifndef FS_UTIL_META_H_
-#define FS_UTIL_META_H_
+#ifndef FS_UTIL_FILE_H_
+#define FS_UTIL_FILE_H_
+
+#include <stdlib.h>
+#include <sys/stat.h>
 
 /**
- * Flush dirty metadata blocks to disk.
- */
-void flush_metadata(void);
-
-/**
- * Gets a free block number from the free list.
+ * Returns the block number of the n-th block of the file,
+ * or allocates it if it does not exist and alloc == 1. If
+ * file was extended, the new block is initialized with 0s.
  *
- * @return free block number or 0 if none available
+ * @param inum the number of file inode
+ * @param n the 0-based block index in file
+ * @param alloc 1=allocate block if does not exist 0 = fail
+ *   if does not exist
+ * @return block number of the n-th block or 0 if unavailable
  */
-int get_free_blk(void);
+int get_file_blkno(int inum, int n, int alloc);
 
 /**
- * Return a block to the free list.
+ * Gets the n-th block of the file, or allocates it if it
+ * does not exist and alloc == 1. If file was extended, new
+ * block is initialized with 0s. If block is NULL, equivalent
+ * to get_file_blkno().
  *
- * @param  blkno the block number
+ * Errors
+ *   -EIO  - error reading block
+ *
+ * @param inum the number of file inode
+ * @param n the 0-based block index in file
+ * @param block storage for the block read
+ * @param alloc 1=allocate block if does not exist 0 = fail
+ *   if does not exist
+ * @return block number of the n-th block, 0 if unavailable,
+ *   or -error number
  */
-void return_blk(int blkno);
+int get_file_blk(int inum, int n, void* block, int alloc);
 
 /**
- * Determines whether block with blkno is free.
+ * Read bytes from content of an inode.
  *
- * @param blkno the block number
- * @return 1 (true) or 0 (false)
+ * Should return exactly the number of bytes requested, except:
+ *   - if offset >= file len, return 0
+ *   - if offset+len > file len, return bytes from offset to EOF
+ *   - on error, return <0
+ *
+ * Errors:
+ *   -EIO     - error reading block
+ *
+ * @param inum the inumber of inode to truncate
+ * @param buf the read buffer
+ * @param len the number of bytes to read
+ * @param offset to start reading at
+ * @return number of bytes actually read if successful, or -error number
  */
-int is_free_blk(int blkno);
+int do_read(int inum, char* buf, size_t len, off_t offset);
+
+/**
+ *  write bytes of content to an inode.
+ *
+ * It should return exactly the number of bytes requested, except on
+ * error.
+ *
+ * Errors:
+ *   -ENOSPC  - no space in file sysem
+ *   -EINVAL  - if 'offset' is greater than current file length.
+ *  			(POSIX semantics support the creation of files with
+ *  			"holes" in them, but we don't)
+ *
+ * @param inum the inumber of inode to truncate
+ * @param buf the buffer to write
+ * @param len the number of bytes to write
+ * @param offset the offset to starting writing at
+ * @return number of bytes actually written if successful, or -error number
+ */
+int do_write(int inum, const char* buf, size_t len, off_t offset);
 
 
 /**
- * Gets a free inode number from the free list.
+ * Truncate fuse_parser_data specified by inode.
+ * Currently only length 0 is supported.
  *
- * @return a free inode number or 0 if none available
+ * Errors
+ *   -EINVAL  - invalid length argument
+ *
+ * @param inum the inumber of inode to truncate
+ * @param len new length of file -- currently only 0 allowed
+ * @return 0 if successful, or -error number
  */
-int get_free_inode(void);
+int do_truncate(int inum, int len);
 
 /**
- * Return a inode to the free list.
+ * Rename a file or directory.
  *
- * @param  inum the inode number
+ * Note that this is a simplified version of the UNIX rename
+ * functionality - see 'man 2 rename' for full semantics. In
+ * particular, the full version can move across directories, replace a
+ * destination file, and replace an empty directory with a full one.
+ *
+ * Errors:
+ *   -ENOENT   - source file or directory does not exist
+ *   -ENOTDIR  - component of source or target path not a directory
+ *   -EEXIST   - destination already exists
+ *   -EINVAL   - source and destination not in the same directory
+ *
+ * @param src_path the source path
+ * @param dst_path the destination path.
+ * @return 0 if successful, or -error number
  */
-void return_inode(int inum);
+int do_rename(int srcdir_inum, const char* src_leaf,
+              int dstdir_inum, const char* dst_leaf);
 
 /**
- * Determines whether inode with inum is free.
+ * Fill in a stat structure for an inode.
  *
- * @param inum the inode number
- * @return 1 (true) or 0 (false)
+ * @param inum inode number
+ * @param sb pointer to stat structure
  */
-int is_free_inode(int inum);
-
+void do_stat(int inum, struct stat *sb);
 
 /**
- * Mark a inode as dirty.
+ * Allocate and initialize new inode with mode and file type.
  *
- * @param inum the number of inode to mark dirty
+ *  * Errors
+ *   -ENOSPC   - free inode not available
+ *
+ * @param mode the mode, indicating block or character-special file
+ * @param ftype the type of file (S_IFDIR for dir, S_IFREG for regular)
+ * @return inum of entry if successful, -error if error
  */
-void mark_inode(int inum);
+int init_new_inode(mode_t mode, unsigned ftype);
+
+/**
+ * Make a file system file or directory entry for the file type.
+ *
+ * Note: use GUD and UID from fuse_context returned
+ * by fuse get_fuse_context() function
+ *
+ * Errors
+ *   -ENOTDIR  - dir inode is not a directory
+ *   -EEXIST   - directory already exists
+ *   -ENOSPC   - free inode not available
+ *   -ENOSPC   - results in >32 entries in directory
+ *
+ * @param dir_inum the inum of the directory inode
+ * @param leaf the leaf name in the inode directory
+ * @param mode the mode, indicating block or character-special file
+ * @param ftype the type of file (S_IFDIR for dir, S_IFREG for regular)
+ * @return inum of entry if successful, -error if error
+ */
+int do_mkentry(int dir_inum, const char* leaf, mode_t mode, unsigned ftype);
+
+/**
+ * Remove an file named leaf in the specified
+ * directory inum.
+ *
+ * Errors
+ *   -ENOENT   - file does not exist
+ *   -ENOTDIR  - component of path not a directory
+ *   -EISDIR    - path is a directory
+ *   -ENOTEMPTY - directory not empty
+ *
+ * @param dir_inum the inumber of a directory inode
+ * @param leaf the name of a child directory entry
+ * @return 0 if successful, or -error number
+ */
+int do_unlink(int dir_inum, const char* leaf);
 
 
-#endif /* FS_UTIL_META_H_ */
+int do_link(int src_inum, const char* dir, const char* leaf);
+
+#endif /* FS_UTIL_FILE_H_ */
